05c358c79de5c20172a85615edd92c7dfa300a2a
Sara Kaoukabi <sara.kaoukabi@cognite.com>
2020-10-12 14:31:24 +0200

    Added input() vulnerability in code.py

--- code.py
+++ code.py
@@ -1,10 +1,18 @@
 def addition(a, b):
-  return eval("%s + %s" % (a, b))
+    return eval("%s + %s" % (a, b))
 
+def minus(a, b):
+    return a-b
 
 if __name__ == "__main__":
 
     func_input = {"a":"1", "b":"2"}
-    result = addition(func_input['a'], func_input['b'])
-    
-    print("The result is %d." % result)
+    result1 = addition(func_input['a'], func_input['b'])
+
+    print("The addition result is %d." % result1)
+
+    a = int(input("a: "))
+    b = int(input("b: "))
+    result2 = minus(a,b)
+
+    print("The minus result is %d." % result2)
