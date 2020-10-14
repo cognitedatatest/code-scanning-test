d6cb4ed1ec783c58aa8f3ba5783412317f12306a
Sara Kaoukabi <sara.kaoukabi@cognite.com>
2020-10-14 10:34:36 +0200

    Changes in code.py

--- code.py
+++ code.py
@@ -1,12 +1,14 @@
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
-from database.users import User
 
-@app.route('hello')
-def hello():
-    id = request.args.get("id")
-    stmt = text("SELECT * FROM users where id=%s" % id) # Query is constructed based on user inputs
-    query = SQLAlchemy().session.query(User).from_statement(stmt) # Noncompliant
-    user = query.one()
-    return "Hello %s" % user.username
+from flask import Flask, request, redirect
+import re
+
+app = Flask(__name__)
+
+UNSAFE_REGEX = re.compile("(www|beta).example.com/")
+SAFE_REGEX = re.compile(r"(www|beta)\.example\.com/")
+
+@app.route('/some/path/bad')
+def unsafe(request):
+    target = request.args.get('target', '')
+    if UNSAFE_REGEX.match(target):
+        return redirect(target)
