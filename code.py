34201a4c64076ce004c33a8c64a2c7e8b1f651b0
Sara Kaoukabi <sara.kaoukabi@cognite.com>
2020-10-14 10:43:04 +0200

    Testing Incomplere Hostname RegExp vulnerability

--- code.py
+++ code.py
@@ -1,4 +1,13 @@
-import obvious.txt
+from flask import Flask, request, redirect
+import re
 
-if __name__ == "__main__":
-    print("Secret token : %s",token)
+app = Flask(__name__)
+
+UNSAFE_REGEX = re.compile("(www|beta).example.com/")
+SAFE_REGEX = re.compile(r"(www|beta)\.example\.com/")
+
+@app.route('/some/path/bad')
+def unsafe(request):
+    target = request.args.get('target', '')
+    if UNSAFE_REGEX.match(target):
+        return redirect(target)
